4cc657ece675a5a02f9fa8654e2c6ea54877b7eb
asnich <asitha93@live.com>
2018-02-21 15:30:05 +0530

    Fixed #1

--- extendz-spring-boot-example/src/main/java/club/extendz/spring/example/modules/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package club.extendz.spring.example.modules;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends PagingAndSortingRepository<User, Long> {
-
-}
