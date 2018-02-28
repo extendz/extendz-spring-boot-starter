86b197fb5effe7b3dbf856b50b94fe42967a5a87
Rumes Shyaman <rs.kumarasinghe89@gmail.com>
2018-02-28 16:05:56 +0530

    cors set

--- extendz-spring-boot-example/src/main/java/club/extendz/spring/example/ExtendzSpringBootExampleApplication.java
+++ extendz-spring-boot-example/src/main/java/club/extendz/spring/example/ExtendzSpringBootExampleApplication.java
@@ -15,13 +15,43 @@
  */
 package club.extendz.spring.example;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication(scanBasePackages = "club.extendz")
 public class ExtendzSpringBootExampleApplication {
+	
+	private Environment environment;
+	
+	public ExtendzSpringBootExampleApplication(Environment environment) {
+		this.environment = environment;
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExtendzSpringBootExampleApplication.class, args);
+	}
+	
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+            public void addCorsMappings(CorsRegistry registry) {
+				if(Arrays.toString(environment.getActiveProfiles()).contains("prod")) {
+					registry.addMapping("/api").allowedOrigins("https://extendz-springboot.herokuapp.com");
+				}else {
+					registry.addMapping("/api").allowedOrigins("*");
+				}
+                
+            }
+		};
 	}
 }
